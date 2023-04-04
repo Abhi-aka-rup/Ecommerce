9e531cf26fb39a423ec76ea32e5bf0b398463157
Abhirup <rup.mi2010@gmail.com>
2023-04-04 11:17:46 +0530

    Add project files.

---
+++ Products/Src/Core/Domain/Common/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common
+{
+    public class AuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string? LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
